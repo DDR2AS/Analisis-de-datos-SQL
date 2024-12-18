59df157cd156076f1630e2fbc82d048252a4d72f
Diego Avila Saldaña <71202762+DDR2AS@users.noreply.github.com>
2024-12-17 22:55:01 -0500

    Add files via upload

---
+++ Clases/Clase-4/PARTE2_CLASE_DML.sql
@@ -0,0 +1,3 @@
+-----------------LENGUAJE DML
+---JOINS-------------UNION-------------EXCEPT
+select gestion.CodigoProducto,GESTION_DETALLE.DetalleResultadofrom GESTION left join gestion_detalleon gestion.resultadogestion = gestion_detalle.ResultadoGestion
