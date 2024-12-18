ba3b5d8536cb0ded25cecc44279ff12e3d0bd6d1
Diego Avila Saldaña <71202762+DDR2AS@users.noreply.github.com>
2024-12-17 22:57:35 -0500

    Update and rename PARTE2_CLASE_DML.sql to Joins.sql

--- Clases/Clase-4/PARTE2_CLASE_DML.sql
+++
@@ -1,3 +0,0 @@
------------------LENGUAJE DML
----JOINS-------------UNION-------------EXCEPT
-select gestion.CodigoProducto,GESTION_DETALLE.DetalleResultadofrom GESTION left join gestion_detalleon gestion.resultadogestion = gestion_detalle.ResultadoGestion
